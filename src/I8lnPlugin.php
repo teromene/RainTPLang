c5b706dd0e9b14403fd89670c326cf472a3be5b1
Teromene <teromene@teromene.fr>
2016-11-16 13:14:09 +0000

    First commit.

---
+++ src/I8lnPlugin.php
@@ -0,0 +1,29 @@
+<?php
+namespace Rain\Tpl\Plugin;
+
+class I8lnPlugin extends \Rain\Tpl\Plugin{
+
+    protected $hooks = array('beforeParse');
+
+	protected static $regex = '/{lang=(["\'].*["\'])}/m';
+	protected static $i8lnFunction = '_';
+
+    public function beforeParse(\ArrayAccess $context){
+
+		$context->code = preg_replace(self::$regex, self::getReplacement(), $context->code);
+
+    }
+
+	public static function setI8lnFunction($functionName) {
+
+		self::$i8lnFunction = $functionName;
+
+	}
+
+	public static function getReplacement() {
+
+		return '<?php echo ' . self::$i8lnFunction . '($1); ?>';
+
+	}
+
+}
---
+++ tests/I8lnPluginTest.php
@@ -0,0 +1,74 @@
+<?php
+
+require "../vendor/autoload.php";
+require_once "../src/I8lnPlugin.php";
+
+use Rain\Tpl;
+
+class I8lnPluginTest extends phpunit\framework\TestCase {
+
+	public static $TPLBaseText = '<?php if(!class_exists(\'Rain\Tpl\')){exit;}?>';
+
+	private static function getTemplateString($templateName) {
+
+		$t = new Tpl;
+		$t->draw($templateName, TRUE);
+
+		$filename = glob('cache/*.php')[0];
+		$content = file_get_contents($filename);
+		unlink($filename);
+
+		return $content;
+
+	}
+
+	public static function setUpBeforeClass() {
+
+		$config = array(
+			"tpl_dir" => "./",
+			"cache_dir" => "cache/"
+		);
+
+		Tpl::configure($config);
+		Tpl::registerPlugin( new Tpl\Plugin\I8lnPlugin() );
+
+	}
+
+	public function testDefaultFunctionSingleQuotes() {
+
+		$expectedString = self::$TPLBaseText . "<?php echo _('Hello world !'); ?>\n\n";
+		$this->assertEquals($expectedString, self::getTemplateString("testSingleQuotes"), 'The expected value for default function single quotes is wrong !'); 
+
+
+	}
+
+	public function testDefaultFunctionDoubleQuotes() {
+
+		$expectedString = self::$TPLBaseText . "<?php echo _(\"Hello world !\"); ?>\n\n";
+		$this->assertEquals($expectedString, self::getTemplateString("testDoubleQuotes"), 'The expected value for default function single quotes is wrong !'); 
+
+
+	}
+
+	public function testCustomFunctionSingleQuotes() {
+
+		Tpl\Plugin\I8lnPlugin::setI8lnFunction("gettext");
+
+		$expectedString = self::$TPLBaseText . "<?php echo gettext('Hello world !'); ?>\n\n";
+		$this->assertEquals($expectedString, self::getTemplateString("testSingleQuotes"), 'The expected value for default function single quotes is wrong !'); 
+
+
+	}
+
+	public function testCustomFunctionDoubleQuotes() {
+
+		Tpl\Plugin\I8lnPlugin::setI8lnFunction("gettext");
+
+		$expectedString = self::$TPLBaseText . "<?php echo gettext(\"Hello world !\"); ?>\n\n";
+		$this->assertEquals($expectedString, self::getTemplateString("testDoubleQuotes"), 'The expected value for default function single quotes is wrong !'); 
+
+
+	}
+
+}
+?>
